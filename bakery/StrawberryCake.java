a179ee129f146b874f84888f7ba4dca748a3026a
David Soares <137106233+Soa1804@users.noreply.github.com>
2026-09-23 20:22:51 -0300

    Adiciona a classe StrawberryCake com custo em dobro

--- bakery/StrawberryCake.java
+++ bakery/StrawberryCake.java
@@ -1,5 +1,17 @@
 package bakery;
 
-public class StrawberryCake {
-    
+/**
+ * Novo tipo de bolo: morango. Custa o dobro de um bolo padrao.
+ * Nenhuma classe existente precisou ser alterada para cria-lo.
+ */
+public class StrawberryCake extends Cake {
+    @Override
+    public int getCost() {
+        return super.getCost() * 2;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Strawberry cake";
+    }
 }
